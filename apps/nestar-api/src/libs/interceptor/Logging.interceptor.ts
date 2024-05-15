39b8cbc568c117e82e769e1c6ea1fe10ff6cac59
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-05-15 10:44:14 +0900

    feat: develop logiing via global interceptors

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,17 +1,35 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from "@nestjs/common";
+import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor{
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        console.log('Before...');
-        
-        const now = Date.now();
-        return next
-        .handle()
-        .pipe(
-            tap(() => console.log(`After... ${Date.now() - now}ms`))
+    private readonly logger: Logger = new Logger();
+
+    public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const recordTime = Date.now();
+        const requestType = context.getType<GqlContextType>();
+
+        if(requestType === 'http') {
+            // Develop logic for rest api if needed
+        } else if (requestType === 'graphql') {
+            // (1) Print Request
+        const GqlContext = GqlExecutionContext.create(context);
+        this.logger.log(`${this.stringify(GqlContext.getContext().req.body)}`, 'REQUEST');
+
+            // (2) Error handling via GraphQL
+            // (3) If no Errors giving Response below
+        return next.handle().pipe(
+            tap((context) => {
+                const responseTime = Date.now() - recordTime;
+                this.logger.log(`${this.stringify(context)} - ${responseTime}ms\n\n`, 'RESPONSE')
+            }),
         );
+        }
+    }
+
+    private stringify(context: ExecutionContext): string {
+        return JSON.stringify(context).slice(0, 75)
     }
 }
