acf9ef12c1bffdd8052fa32a342b121cee1e7210
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-05-17 17:53:24 +0900

    feat: create checkAuthRoles for testing

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -6,6 +6,9 @@ import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class MemberResolver {
@@ -41,18 +44,25 @@ export class MemberResolver {
         return ` Hi ${memberNick}`;    
     }
 
+    @Roles(MemberType.USER, MemberType.AGENT)
+    @UseGuards(RolesGuard)
+    @Query(() => String)
+    public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+        console.log("Query: checkAuthRoles");
+        return ` Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;    
+    }
+
     @Query(() => String)
     public async getMember(): Promise<string> {
         console.log("Mutation: getMember");
         return this.memberService.getMember();    
     }
 
-
         // ADMIN
-
-        // Authoritation : Admin
+        @Roles(MemberType.ADMIN)
+        @UseGuards(RolesGuard)
         @Mutation(() => String)
-        public async getAllMembersByAdmin(): Promise<string> {
+        public async getAllMembersByAdmin(): Promise<string> {            
             return this.memberService.getAllMembersByAdmin();    
         }
 
