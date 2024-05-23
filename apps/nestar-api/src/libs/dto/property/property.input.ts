0150a08f3c9fc9f1fda93b50af8f18b34878cddb
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-05-24 00:39:56 +0900

    feat: develop getAllProprties graphQL api

--- apps/nestar-api/src/libs/dto/property/property.input.ts
+++ apps/nestar-api/src/libs/dto/property/property.input.ts
@@ -204,3 +204,41 @@ class PISearch{
         @Field(() => APISearch)
         search: APISearch;
     }
+
+    @InputType()
+    class ALPSearch{
+        @IsOptional()
+        @Field(() => PropertyStatus, {nullable:true})
+        propertyStatus?: PropertyStatus;
+
+        @IsOptional()
+        @Field(() => [PropertyLocation], {nullable:true})
+        propertyLocationList?: PropertyLocation[];        
+    }
+
+    @InputType()
+    export class AllPropertiesInquiry{
+        @IsNotEmpty()
+        @Min(1)
+        @Field(() => Int)
+        page: number;
+
+        @IsNotEmpty()
+        @Min(1)
+        @Field(() => Int)
+        limit: number;
+
+        @IsOptional()
+        @IsIn(availablePropertySorts)
+        @Field(() => String, {nullable:true})
+        sort?: string;
+
+        @IsOptional()
+        @Field(() => Direction, {nullable: true})
+        direction?: Direction;
+
+        @IsNotEmpty()
+        @Field(() => ALPSearch)
+        search: ALPSearch
+    }
+
