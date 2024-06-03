c358d940c562ab88d826ef3d467aa200b0e56100
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-06-03 10:57:05 +0900

    feat: cerate batchRollBack, batchProperties, batchAgents jobSchedulers logic

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -5,4 +5,17 @@ export class BatchService {
   getHello(): string {
     return 'Welcome to Nestart BATCH Server!';
   }
+
+  public async batchRollBack(): Promise<void> {
+    console.log('batchRollBack');
+  }
+
+  public async batchProperties(): Promise<void> {
+    console.log('batchProperties');
+  }
+  
+  public async batchAgents(): Promise<void> {
+    console.log('batchAgents');
+  }
+
 }
