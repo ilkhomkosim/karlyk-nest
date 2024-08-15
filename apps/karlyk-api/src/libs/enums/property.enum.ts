ab23f7af25a045b81a79394b29607e010854abca
ilkhomkosim <ilkhomqosimov@gmail.com>
2024-08-15 22:17:49 +0900

    feat: modify property enums

--- apps/karlyk-api/src/libs/enums/property.enum.ts
+++ apps/karlyk-api/src/libs/enums/property.enum.ts
@@ -21,15 +21,12 @@ registerEnumType(PropertyStatus, {
 });
 
 export enum PropertyLocation {
-	SEOUL = 'SEOUL',
-	BUSAN = 'BUSAN',
-	INCHEON = 'INCHEON',
-	DAEGU = 'DAEGU',
-	GYEONGJU = 'GYEONGJU',
-	GWANGJU = 'GWANGJU',
-	CHONJU = 'CHONJU',
-	DAEJON = 'DAEJON',
-	JEJU = 'JEJU',
+	Miadong987_456 = 'Miadong987_456',
+	Gangbuk_ku_168 = 'Gangbuk-ku_168',
+	Chungchangte_ro_244 = 'Chungchangte_ro_244',
+	Tongnamgo_168 = 'Tongnamgo_168',
+	Banghakro_23 = 'Banghakro_23',
+	Dongilro_1345 = 'Dongilro_1345',
 }
 registerEnumType(PropertyLocation, {
 	name: 'PropertyLocation',
